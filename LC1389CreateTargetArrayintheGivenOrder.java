1d772ac3278e4f217d6090e0726480c42815c5eb
Pranay Bonagiri <vinaypranay2329@gmail.com>
2026-08-06 23:21:49 +0530

    DAY3:LC1389 - Create Target Array in the Given Order

---
+++ LC1389CreateTargetArrayintheGivenOrder.java
@@ -0,0 +1,40 @@
+// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+// Output: [0,4,1,3,2]
+// Explanation:
+// nums       index     target
+// 0            0        [0]
+// 1            1        [0,1]
+// 2            2        [0,1,2]
+// 3            2        [0,1,3,2]
+// 4            1        [0,4,1,3,2]
+
+import java.util.Arrays;
+
+/**
+ * LC1389CreateTargetArrayintheGivenOrder
+ */
+public class LC1389CreateTargetArrayintheGivenOrder {
+
+    public static void main(String[] args) {
+        
+        int[] nums={0,1,2,3,4};
+        int[] index={0,1,2,2,1};
+
+        int[] arr=new int[nums.length];
+
+        int size=0;
+
+        for(int i=0;i<arr.length;i++){
+
+
+            for(int j=size;j>index[i];j--){
+
+                arr[j]=arr[j-1];
+
+            }
+            arr[index[i]]=nums[i];
+            size++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
