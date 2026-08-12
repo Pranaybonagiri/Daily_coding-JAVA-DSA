d1e4e6700ca6916ba8072bf4f578cf3ea4528519
Pranay Bonagiri <vinaypranay2329@gmail.com>
2026-08-12 19:48:48 +0530

    LeetCode-342. Power Of Four

--- Day006.java
+++ Day006.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 
 public class Day6 {
     public static void main(String[] args) {
-        
+      
         Scanner sc=new Scanner(System.in);
 
         System.out.println("enter number:");
