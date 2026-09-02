b9db550602667eebdc32a61f7fd229896871d67c
Pranay Bonagiri <vinaypranay2329@gmail.com>
2026-09-02 17:38:19 +0530

    Leetcode:3875. Construct Uniform Parity Array I

---
+++ Day023.java
@@ -0,0 +1,22 @@
+
+// Leetcode:3875. Construct Uniform Parity Array I
+
+// Input: nums1 = [2,3]
+
+// Output: true
+
+import java.util.*;
+public class Day023 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of elements: ");
+        int n = sc.nextInt();
+        int[] nums1 = new int[n];
+        System.out.println("enter elements: ");
+        for(int i=0; i<n; i++){
+            nums1[i] = sc.nextInt();
+        }
+       
+        System.out.println(true);
+    }
+}
